93addc31004b6ece3d667b19559ad026903ecf19
jrmarqueshd <jrmarqueshd@gmail.com>
2020-03-19 21:29:51 -0300

    remove console log

--- src/Services/Api/CoronaAnalytic/Requests/index.js
+++ src/Services/Api/CoronaAnalytic/Requests/index.js
@@ -2,6 +2,5 @@ import AnalyticsAPI from "../index";
 
 export async function fetchVirusInfo() {
   const response = await AnalyticsAPI.get();
-  console.log(response.data);
   return response.data;
 }
